0fa64464b867261ba3d0e3239a32d98159ad4596
gustavo <gustavocmangold@gmail.com>
2024-11-13 11:43:03 -0300

    change generate

--- generate-data.sh
+++ generate-data.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 LC_NUMERIC="en_US.UTF-8"
 
-for defects in $(seq 0 500 9000)
+for defects in $(seq 0 250 9000)
 do
-	for mobility in .01 .03 .05 .07 .1 .3 .5 .7 1.
+        for mobility in .01 .03 .05 .07 .1 .3 .5 .7 1.
+
         do
                 echo "1.4 $defects $mobility"
                 ./pris_pd_Qlearning_glut 1.4 $defects $mobility &
