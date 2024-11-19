49074d3a526a032729eca5b0f5edd353a40c47fe
Gustavo Mangold <gustavocmangold@gmail.com>
2024-11-19 00:48:06 -0300

    aasdsad

--- generate-data.sh
+++ generate-data.sh
@@ -3,7 +3,7 @@ LC_NUMERIC="en_US.UTF-8"
 
 for mobility in 0. 0.01 0.05 0.1 0.5 1.
         do
-        for defects in $(seq 0 50 500)
+        for defects in $(seq 9000 50 9500)
                 do
                 echo "1.4 $defects $mobility 0.1"
                 ./pris_pd_Qlearning_glut 1.4 $defects $mobility 0.1 &
@@ -13,7 +13,7 @@ done
 
 for mobility in 0. 0.01 0.05 0.1 0.5 1.
         do
-        for defects in $(seq 500 50 1000)
+        for defects in $(seq 9500 50 10000)
                 do
                 echo "1.4 $defects $mobility 0.1"
                 ./pris_pd_Qlearning_glut 1.4 $defects $mobility 0.1 &
