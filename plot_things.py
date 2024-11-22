d8163fc0bc1f579e4564f818120f89ad1609401d
Gustavo Mangold <gustavocmangold@gmail.com>
2024-11-22 09:56:17 -0300

    change plot and stuff?

--- plot_things.py
+++ plot_things.py
@@ -23,9 +23,10 @@ def plot_heatmap(x_list, y_list, cooperation_list):
     #plt.gca().invert_yaxis()
 
     plt.ylim(0.01, 1.)
+    plt.xlim(0.07, 1.)
     plt.yticks([.1, .2, .3, .4, .5, .6, .7, .8, .9, 1.])
 
-    plt.tricontourf(x, y, z, levels = 70, cmap = 'jet_r')
+    plt.tricontourf(x, y, z, levels = 80, cmap = 'jet_r')
     cbar = plt.colorbar()
     cbar.set_ticks(np.arange(0, 1.01, 0.1))
 
@@ -81,6 +82,8 @@ cooperation_plot = []
 x_static = []
 y_static = []
 
+check_repeat_params = []
+
 index = 0
 for filename in glob.glob(path + 'T*.dat'):
     data = pd.read_csv(filename, comment = '#', delimiter = ' ', names = colnames, index_col = False)
@@ -90,7 +93,7 @@ for filename in glob.glob(path + 'T*.dat'):
     data['mean_coop'] = data['f_c'] / (data['f_d'] + data['f_c'])
 
     try:
-        x_variable  = float(filename.split('rho')[1][:4])
+        x_variable  = float(filename.split('rho')[1][:6])
         mean_coop   = np.mean(data[['mean_coop']].to_numpy()[-100:])
         var_coop    = np.var(data[['mean_coop']].to_numpy()[-100:])
 
@@ -102,16 +105,19 @@ for filename in glob.glob(path + 'T*.dat'):
             x_static.append(x_variable)
             y_static.append(mean_coop)
 
-        if key in (cooperation_dict.keys()):
-            cooperation_dict[key].append([x_variable, float(mean_coop)])
-            variance_dict[key].append([x_variable, float(var_coop)])
-        else:
-            cooperation_dict[key] = [[x_variable, float(mean_coop)]]
-            variance_dict[key] = [[x_variable, float(var_coop)]]
+        if x_variable > 0.001 and (not ([key, x_variable] in check_repeat_params)):
+            labels_to_plot.append(key)
+            x_axis_to_plot.append(x_variable)
+            cooperation_plot.append(mean_coop)
 
-        labels_to_plot.append(key)
-        x_axis_to_plot.append(x_variable)
-        cooperation_plot.append(mean_coop)
+            if key in (cooperation_dict.keys()):
+                cooperation_dict[key].append([x_variable, float(mean_coop)])
+                variance_dict[key].append([x_variable, float(var_coop)])
+            else:
+                cooperation_dict[key] = [[x_variable, float(mean_coop)]]
+                variance_dict[key] = [[x_variable, float(var_coop)]]
+
+            check_repeat_params.append([key, x_variable])
 
         index += 1
     except:
@@ -125,6 +131,10 @@ plt.style.use('seaborn-v0_8-ticks')
 
 marker = itertools.cycle((',', 'P', 'p', '.', '*', 'X', 'P', 'p', 'o'))
 
+###################
+### normal plot ###
+###################
+
 #codigo horrivel, mas funciona
 # tem que dar sorted com relaçao a coordenada x
 # senao o plot fica errado, fora de ordem as conexoes
@@ -137,13 +147,14 @@ for key in sorted(cooperation_dict.keys()):
     if key in [0.01, 0.05, 0.1, 0.5, 1.]:
         color_both_plots = next(color)
         plt.scatter(*zip(*cooperation_dict[key]),  marker = next(marker), linestyle='',
-            label = r'$p_d = $' + str(key), color = color_both_plots)
+            label = r'$p_d = $' + str(key), color = color_both_plots, s = 20)
         #plt.plot(*zip(*cooperation_dict[key]), linewidth = 0.5, alpha=0.4, color = color_both_plots)
         index += 1
 
 plt.title('')
 plt.ylim(-0.01, 1.01)
-plt.xlim(0.09, 1.05)
+plt.xticks(np.arange(0.1, 1, 0.1))
+plt.xlim(0.07, 1.05)
 plt.xlabel(r'$\rho$')
 plt.ylabel(r'$f_c$')
 plt.legend(loc='upper right', ncol = 1, edgecolor = 'black', framealpha=0.5)
@@ -152,6 +163,42 @@ plt.savefig('cooperation_versus_b-per_occupation-fermi.png', dpi=400, bbox_inche
 plt.close()
 plt.clf()
 plt.cla()
+
+marker = itertools.cycle((',', 'P', 'p', '.', '*', 'X', 'P', 'p', 'o'))
+color = itertools.cycle(("#0E56FD", "#6135ca", "#606b9b", "#ca23dc",  "#e61976", "#d02f6a", "#ff1611"))
+
+###################
+### zoom plot   ###
+###################
+plt.plot(x_plot, y_plot, label = r'$p_d = 0$', color = color_plots_static, alpha=0.75, linestyle='dotted')
+
+index = 0
+for key in sorted(cooperation_dict.keys()):
+    if key in [0.05]:
+        color_both_plots = next(color)
+        plt.scatter(*zip(*cooperation_dict[key]),  marker = 'P', linestyle='',
+            label = r'$p_d = $' + str(key), color = "#6135ca")
+        #plt.plot(*zip(*cooperation_dict[key]), linewidth = 0.5, alpha=0.4, color = color_both_plots)
+        index += 1
+
+plt.title('')
+plt.ylim(-0.01, 1.01)
+plt.xlim(0.07, .16)
+plt.xlabel(r'$\rho$')
+plt.ylabel(r'$f_c$')
+#plt.legend(loc='upper right', ncol = 1, edgecolor = 'black', framealpha=0.5)
+plt.savefig('cooperation_versus_b-per_occupation-fermi-zoom.png', dpi=400, bbox_inches='tight')
+
+plt.close()
+plt.clf()
+plt.cla()
+
+###################
+### var plot    ###
+###################
+
+marker = itertools.cycle((',', 'P', 'p', '.', '*', 'X', 'P', 'p', 'o'))
+color = itertools.cycle(("#0E56FD", "#6135ca", "#606b9b", "#ca23dc",  "#e61976", "#d02f6a", "#ff1611"))
 
 index = 0
 for key in sorted(variance_dict.keys()):
