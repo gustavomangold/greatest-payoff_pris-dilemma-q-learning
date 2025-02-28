1e94dca41b3faf4d9b89869bc83dec049df8639a
gustavo <gustavocmangold@gmail.com>
2025-02-28 09:04:09 -0300

    change generate data

--- generate-data.sh
+++ generate-data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 LC_NUMERIC="en_US.UTF-8"
 
-for defects in $(seq 0 50 9000)
+for defects in $(seq 50 50 3000)
 do
-        for mobility in 0
+        for mobility in 0 .02 .05 .07 .1 .12 .15 .17 .2 .23 .25 .3 .32 .35 .4
 
         do
                 echo "1.4 $defects $mobility"
